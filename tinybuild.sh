70f83107319931b6eb6b5fd452147cc662be66e4
Julia Evans <julia@jvns.ca>
2022-01-21 21:54:21 -0500

    some comments that may or may not help my future self

--- tinybuild.sh
+++ tinybuild.sh
@@ -35,13 +35,18 @@ if [ -d "$localdir" ]; then
     exit 1
 fi
 
+# create the directory outside the container so that it's owned by my user to
+# avoid permission issues
 mkdir "$localdir"
 
 DIRNAME=$(basename "$(pwd)")
 IMAGE_NAME="tinybuild-$DIRNAME"
+# Don't send the current directory to the Docker daemon, to speed up builds
 docker build - -t "$IMAGE_NAME" < Dockerfile
 CONTAINER_ID=$(docker run -v "$PWD":/src -v "$localdir:/artifact:z" -d -t "$IMAGE_NAME" /bin/bash)
+# more permission nonsense, so that the git clone on the next line works
 docker exec "$CONTAINER_ID" bash -c "chmod 777 /"
+# set uid to 1000 to avoid permission issues, this is just hardcoded to my uid
 if ! docker exec --user 1000:1000 "$CONTAINER_ID" bash -c "git clone /src /build && cd /build && bash /src/$script"
 then
     echo "Build failed"
